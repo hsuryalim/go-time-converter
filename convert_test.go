29cad192a4e385a93f5c9ba9e4ccca499443cbc7
ts-hanjaya.suryalim <ts-hanjaya.suryalim@rakuten.com>
2023-02-19 13:22:31 +0900

    change test case

--- convert_test.go
+++ convert_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestConvert(t *testing.T) {
-	actual := Convert(15)
-	expected := 15
+	actual := Convert(10)
+	expected := 10
 	assert.Equal(t, expected, actual, fmt.Sprintf("Expected to be %v", expected))
 }
